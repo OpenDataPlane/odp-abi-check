aad8e2e867044f8217ca6f36b04fe6fa7d4f2935
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-28 15:02:22 +0000

    Deploy Linaro/odp-abi-check to github.com/Linaro/odp-abi-check.git:installed-out

---
+++ ODP/1.21.0.0/include/odp/api/spec/init.h
@@ -0,0 +1,349 @@
+/* Copyright (c) 2013-2018, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+
+/**
+ * @file
+ *
+ * ODP initialization.
+ * ODP requires a global level init for the process and a local init per
+ * thread before the other ODP APIs may be called.
+ * - odp_init_global()
+ * - odp_init_local()
+ *
+ * For a graceful termination the matching termination APIs exit
+ * - odp_term_global()
+ * - odp_term_local()
+ */
+
+#ifndef ODP_API_SPEC_INIT_H_
+#define ODP_API_SPEC_INIT_H_
+#include <odp/visibility_begin.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp/api/std_types.h>
+#include <odp/api/hints.h>
+#include <odp/api/feature.h>
+#include <odp/api/thread.h>
+#include <odp/api/cpumask.h>
+
+/** @defgroup odp_initialization ODP INITIALIZATION
+ *  Initialisation operations.
+ *  @{
+ */
+
+/**
+ * @typedef odp_instance_t
+ * ODP instance ID.
+ */
+
+/**
+ * ODP log level.
+ */
+typedef enum {
+	ODP_LOG_DBG,
+	ODP_LOG_ERR,
+	ODP_LOG_UNIMPLEMENTED,
+	ODP_LOG_ABORT,
+	ODP_LOG_PRINT
+} odp_log_level_t;
+
+/**
+ * ODP log function
+ *
+ * Instead of direct prints to stdout/stderr all logging in an ODP
+ * implementation should be done via this function or its wrappers.
+ *
+ * The application can provide this function to the ODP implementation in two
+ * ways:
+ *
+ * - A callback passed in via in odp_init_t and odp_init_global()
+ * - By overriding the ODP implementation default log function
+ * odp_override_log().
+ *
+ * @warning The latter option is less portable and GNU linker dependent
+ * (utilizes function attribute "weak"). If both are defined, the odp_init_t
+ * function pointer has priority over the override function.
+ *
+ * @param level   Log level
+ * @param fmt     printf-style message format
+ *
+ * @return The number of characters logged on success
+ * @retval <0 on failure
+ */
+int odp_override_log(odp_log_level_t level, const char *fmt, ...);
+
+/**
+ * ODP abort function
+ *
+ * Instead of directly calling abort, all abort calls in the implementation
+ * should be done via this function or its wrappers.
+ *
+ * The application can provide this function to the ODP implementation in two
+ * ways:
+ *
+ * - A callback passed in via odp_init_t and odp_init_global()
+ * - By overriding the ODP implementation default abort function
+ *   odp_override_abort().
+ *
+ * @warning The latter option is less portable and GNU linker dependent
+ * (utilizes function attribute "weak"). If both are defined, the odp_init_t
+ * function pointer has priority over the override function.
+ *
+ * @warning this function shall not return
+ */
+void odp_override_abort(void) ODP_NORETURN;
+
+/** Replaceable logging function */
+typedef int (*odp_log_func_t)(odp_log_level_t level, const char *fmt, ...);
+
+/** Replaceable abort function */
+typedef void (*odp_abort_func_t)(void) ODP_NORETURN;
+
+/**
+ * Application memory model
+ */
+typedef enum {
+	/** Thread memory model: by default all memory is shareable between
+	 *  threads.
+	 *
+	 *  Within a single ODP instance all ODP handles and pointers to ODP
+	 *  allocated data may be shared amongst threads independent of data
+	 *  allocation time (e.g. before or after thread creation). */
+	ODP_MEM_MODEL_THREAD = 0,
+
+	/** Process memory model: by default all memory is not shareable between
+	 *  processes.
+	 *
+	 *  Within a single ODP instance all ODP handles and pointers to ODP
+	 *  allocated data (excluding non-single VA SHM blocks) may be shared
+	 *  amongst processes independent of data allocation time (e.g. before
+	 *  or after fork).
+	 *
+	 * @see ODP_SHM_SINGLE_VA
+	 */
+	ODP_MEM_MODEL_PROCESS
+
+} odp_mem_model_t;
+
+/**
+ * Global initialization parameters
+ *
+ * These parameters may be used at global initialization time to configure and
+ * optimize ODP implementation to match the intended usage. Application
+ * specifies maximum resource usage. Implementation may round up resource
+ * reservations as needed. Initialization function returns a failure if resource
+ * requirements are too high. Init parameters may be used also to override
+ * logging and abort functions.
+ *
+ * Use odp_init_param_init() to initialize the parameters into their default
+ * values. Unused parameters are left to default values.
+ */
+typedef struct odp_init_t {
+	/** Maximum number of worker threads the user will run concurrently.
+	    Valid range is from 0 to platform specific maximum. Set both
+	    num_worker and num_control to zero for default number of threads. */
+	int num_worker;
+
+	/** Maximum number of control threads the user will run concurrently.
+	    Valid range is from 0 to platform specific maximum. Set both
+	    num_worker and num_control to zero for default number of threads. */
+	int num_control;
+
+	/** Pointer to bit mask mapping CPUs available to this ODP instance
+	    for running worker threads.
+	    Initialize to a NULL pointer to use default CPU mapping.
+	    When the mask is defined, odp_cpumask_default_worker()
+	    uses it instead of returning a default mask.
+	    Applications code should not access this cpumask directly.
+	    Valid range of CPUs and optimal CPU selection
+	    are platform specific, but generally it is recommended that:
+		* worker CPUs are dedicated to run only ODP worker threads
+		  (one thread per CPU)
+		* worker and control masks do not overlap
+		* different ODP instances do not specify overlapping
+		  worker masks
+	 */
+	const odp_cpumask_t *worker_cpus;
+
+	/** Pointer to bit mask mapping CPUs available to this ODP instance
+	    for running control threads.
+	    Initialize to a NULL pointer to use default CPU mapping.
+	    When the mask is defined, odp_cpumask_default_control()
+	    uses it instead of returning a default mask.
+	    Applications code should not access this cpumask directly.
+	    Valid range of CPUs and optimal CPU selection
+	    are platform specific, but generally it is recommended that
+	    worker and control masks do not overlap.
+	 */
+	const odp_cpumask_t *control_cpus;
+
+	/** Replacement for the default log fn */
+	odp_log_func_t log_fn;
+
+	/** Replacement for the default abort fn */
+	odp_abort_func_t abort_fn;
+
+	/** Unused features. These are hints to the ODP implementation that
+	 * the application will not use any APIs associated with these
+	 * features. Implementations may use this information to provide
+	 * optimized behavior. Results are undefined if applications assert
+	 * that a feature will not be used and it is used anyway.
+	 */
+	odp_feature_t not_used;
+
+	/** Application memory model. The main application thread has to call
+	 *  odp_init_global() and odp_init_local() before creating threads that
+	 *  share ODP data. The default value is ODP_MEM_MODEL_THREAD.
+	 */
+	odp_mem_model_t mem_model;
+
+	/** Shared memory parameters */
+	struct {
+		/** Maximum memory usage in bytes. This is the maximum
+		 *  amount of shared memory that application will reserve
+		 *  concurrently. Use 0 when not set. Default value is 0.
+		 */
+		uint64_t max_memory;
+	} shm;
+
+} odp_init_t;
+
+/**
+ * Initialize the odp_init_t to default values for all fields
+ *
+ * @param[out] param Address of the odp_init_t to be initialized
+ */
+void odp_init_param_init(odp_init_t *param);
+
+/**
+ * @typedef odp_platform_init_t
+ * ODP platform initialization data
+ *
+ * @note ODP API does nothing with this data. It is the underlying
+ * implementation that requires it and any data passed here is not portable.
+ * It is required that the application takes care of identifying and
+ * passing any required platform specific data.
+ */
+
+
+/**
+ * Global ODP initialization
+ *
+ * This function must be called once (per instance) before calling any other
+ * ODP API functions. A successful call creates a new ODP instance into the
+ * system and outputs a handle for it. The handle is used in other calls
+ * (e.g. odp_init_local()) as a reference to the instance. When user provides
+ * configuration parameters, the platform may configure and optimize the
+ * instance to match user requirements. A failure is returned if requirements
+ * cannot be met.
+ *
+ * Configuration parameters are divided into standard and platform specific
+ * parts. Standard parameters are supported by any ODP platform, where as
+ * platform specific parameters are defined outside of the ODP API
+ * specification. In addition to 'platform_params' there may be other platform
+ * specific configuration options available (e.g. environmental variables or
+ * a configuration file), but when the application passes 'platform_params',
+ * it should always supersede any other configuration method.
+ *
+ * @param[out] instance   Instance handle pointer for output
+ * @param      params     Standard configuration parameters for the instance.
+ *                        Use NULL to set all parameters to their defaults.
+ * @param platform_params Platform specific configuration parameters.
+ *                        The definition and usage is platform specific.
+ *                        Use NULL to set all parameters to their defaults.
+ *
+ * @retval 0 on success
+ * @retval <0 on failure
+ *
+ * @see odp_term_global()
+ * @see odp_init_local() which is required per thread before use.
+ */
+int odp_init_global(odp_instance_t *instance,
+		    const odp_init_t *params,
+		    const odp_platform_init_t *platform_params);
+
+/**
+ * Global ODP termination
+ *
+ * This function is the final ODP call made when terminating
+ * an ODP application in a controlled way. It cannot handle exceptional
+ * circumstances. In general it calls the API modules terminate functions in
+ * the reverse order to that which the module init functions were called
+ * during odp_init_global().
+ *
+ * This function must be called only after all threads of the instance have
+ * executed odp_term_local(). To simplify synchronization between threads
+ * odp_term_local() identifies which one is the last thread of an instance.
+ *
+ * @param instance        Instance handle
+ *
+ * @retval 0 on success
+ * @retval <0 on failure
+ *
+ * @warning The unwinding of HW resources to allow them to be reused without
+ * reseting the device is a complex task that the application is expected to
+ * coordinate. This api may have platform dependent implications.
+ *
+ * @see odp_init_global()
+ * @see odp_term_local() which must have been called prior to this.
+ */
+int odp_term_global(odp_instance_t instance);
+
+/**
+ * Thread local ODP initialization
+ *
+ * All threads must call this function before calling any other ODP API
+ * functions. The instance parameter specifies which ODP instance the thread
+ * joins. A thread may be simultaneously part of single ODP instance only.
+ *
+ * @param instance        Instance handle
+ * @param thr_type        Thread type
+ *
+ * @retval 0 on success
+ * @retval <0 on failure
+ *
+ * @see odp_term_local()
+ * @see odp_init_global() which must have been called prior to this.
+ */
+int odp_init_local(odp_instance_t instance, odp_thread_type_t thr_type);
+
+/**
+ * Thread local ODP termination
+ *
+ * This function is the second to final ODP call made when terminating
+ * an ODP application in a controlled way. It cannot handle exceptional
+ * circumstances. In general it calls the API modules per thread terminate
+ * functions in the reverse order to that which the module init functions were
+ * called during odp_init_local().
+ *
+ * @retval 1 on success and more ODP threads exist
+ * @retval 0 on success and this is the last ODP thread
+ * @retval <0 on failure
+ *
+ * @warning The unwinding of HW resources to allow them to be reused without
+ * reseting the device is a complex task that the application is expected
+ * to coordinate.
+ *
+ * @see odp_init_local()
+ * @see odp_term_global() should be called by the last ODP thread before exit
+ * of an application.
+ */
+int odp_term_local(void);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <odp/visibility_end.h>
+#endif
---
+++ ODP/1.21.0.0/include/odp/api/spec/packet_flags.h
@@ -0,0 +1,540 @@
+/* Copyright (c) 2014-2018, Linaro Limited
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ */
+
+
+/**
+ * @file
+ *
+ * ODP packet flags
+ */
+
+#ifndef ODP_API_SPEC_PACKET_FLAGS_H_
+#define ODP_API_SPEC_PACKET_FLAGS_H_
+#include <odp/visibility_begin.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <odp/api/std_types.h>
+#include <odp/api/packet.h>
+
+/** @addtogroup odp_packet
+ *  Operations on packet metadata flags.
+ *  @{
+ */
+
+/**
+ * Check for all errors in packet
+ *
+ * Check if packet parsing has found any errors in the packet. The level of
+ * error checking depends on the parse configuration (e.g. included layers and
+ * checksums). Protocol layer functions (e.g. odp_packet_has_l3()) indicate
+ * which layers have been checked, and layer error functions
+ * (e.g. odp_packet_has_l3_error()) which layers have errors.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet has errors
+ * @retval 0           No errors were found
+ */
+int odp_packet_has_error(odp_packet_t pkt);
+
+/**
+ * Check for errors in layer 2
+ *
+ * When layer 2 is included in the parse configuration, check if any errors were
+ * found in layer 2 of the packet.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet has errors in layer 2
+ * @retval 0           No errors were found in layer 2
+ */
+int odp_packet_has_l2_error(odp_packet_t pkt);
+
+/**
+ * Check for errors in layer 3
+ *
+ * When layer 3 is included in the parse configuration, check if any errors were
+ * found in layer 3 of the packet.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet has errors in layer 3
+ * @retval 0           No errors found in layer 3
+ */
+int odp_packet_has_l3_error(odp_packet_t pkt);
+
+/**
+ * Check for errors in layer 4
+ *
+ * When layer 4 is included in the parse configuration, check if any errors were
+ * found in layer 4 of the packet.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet has errors in layer 4
+ * @retval 0           No errors were found in layer 4
+ */
+int odp_packet_has_l4_error(odp_packet_t pkt);
+
+/**
+ * Check for layer 2 protocols
+ *
+ * When layer 2 is included in the parse configuration, check if packet parsing
+ * has found and checked a layer 2 protocol (e.g. Ethernet) in the packet.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    A layer 2 protocol header was found and checked
+ * @retval 0           No layer 2 protocol was found
+ */
+int odp_packet_has_l2(odp_packet_t pkt);
+
+/**
+ * Check for layer 3 protocols
+ *
+ * When layer 3 is included in the parse configuration, check if packet parsing
+ * has found and checked a layer 3 protocol (e.g. IPv4, IPv6) in the packet.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    A layer 3 protocol header was found and checked
+ * @retval 0           No layer 3 protocol was found
+ */
+int odp_packet_has_l3(odp_packet_t pkt);
+
+/**
+ * Check for layer 4 protocols
+ *
+ * When layer 4 is included in the parse configuration, check if packet parsing
+ * has found and checked a layer 4 protocol (e.g. UDP, TCP, SCTP) in the packet.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    A layer 4 protocol header was found and checked
+ * @retval 0           No layer 4 protocol was found
+ */
+int odp_packet_has_l4(odp_packet_t pkt);
+
+/**
+ * Check for Ethernet header
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains an Ethernet header
+ * @retval 0           Packet does not contain an Ethernet header
+ */
+int odp_packet_has_eth(odp_packet_t pkt);
+
+/**
+ * Check for Ethernet broadcast address
+ *
+ * ODP recognizes the destination MAC address FF:FF:FF:FF:FF:FF as
+ * a broadcast address. All others are considered non-broadcast.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Ethernet destination address is the broadcast address
+ * @retval 0           Ethernet destination address is not the broadcast address
+ */
+int odp_packet_has_eth_bcast(odp_packet_t pkt);
+
+/**
+ * Check for Ethernet multicast address
+ *
+ * ODP recognizes the destination MAC address as multicast if bit 7 is 1.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Ethernet destination address is a multicast address
+ * @retval 0           Ethernet destination address is not a multicast address
+ */
+int odp_packet_has_eth_mcast(odp_packet_t pkt);
+
+/**
+ * Check for jumbo frame
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet is a jumbo frame
+ * @retval 0           Packet is not a jumbo frame
+ */
+int odp_packet_has_jumbo(odp_packet_t pkt);
+
+/**
+ * Check for VLAN
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains a VLAN header
+ * @retval 0           Packet does not contain a VLAN header
+ */
+int odp_packet_has_vlan(odp_packet_t pkt);
+
+/**
+ * Check for VLAN QinQ (stacked VLAN)
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains a VLAN QinQ header
+ * @retval 0           Packet does not contain a VLAN QinQ header
+ */
+int odp_packet_has_vlan_qinq(odp_packet_t pkt);
+
+/**
+ * Check for ARP
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains an ARP message
+ * @retval 0           Packet does not contain an ARP message
+ */
+int odp_packet_has_arp(odp_packet_t pkt);
+
+/**
+ * Check for IPv4
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains an IPv4 header
+ * @retval 0           Packet does not contain an IPv4 header
+ */
+int odp_packet_has_ipv4(odp_packet_t pkt);
+
+/**
+ * Check for IPv6
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains an IPv6 header
+ * @retval 0           Packet does not contain an IPv6 header
+ */
+int odp_packet_has_ipv6(odp_packet_t pkt);
+
+/**
+ * Check for IP broadcast address
+ *
+ * For IPv4, ODP recognizes the destination IP address 255.255.255.255 as
+ * a broadcast address. All other addresses are considered non-broadcast.
+ *
+ * For IPv6, no destination addresses are recognized as broadcast addresses.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    IP destination address is a broadcast address
+ * @retval 0           IP destination address is not a broadcast address
+ */
+int odp_packet_has_ip_bcast(odp_packet_t pkt);
+
+/**
+ * Check for IP multicast address
+ *
+ * For IPv4 ODP recognizes destination IP addresses in the range 224.0.0.0
+ * through 239.255.255.255 as multicast addresses.
+ *
+ * For IPv6 ODP recognizes destination IP addresses with prefixes FF00::
+ * through FFFF:: as multicast addresses.
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    IP destination address is a multicast address
+ * @retval 0           IP destination address is not a multicast address
+ */
+int odp_packet_has_ip_mcast(odp_packet_t pkt);
+
+/**
+ * Check for IP fragment
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet is an IP fragment
+ * @retval 0           Packet is not an IP fragment
+ */
+int odp_packet_has_ipfrag(odp_packet_t pkt);
+
+/**
+ * Check for IP options
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains IP options
+ * @retval 0           Packet does not contain IP options
+ */
+int odp_packet_has_ipopt(odp_packet_t pkt);
+
+/**
+ * Check for IPSec
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet requires IPSec processing
+ * @retval 0           Packet does not require IPSec processing
+ */
+int odp_packet_has_ipsec(odp_packet_t pkt);
+
+/**
+ * Check for UDP
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains a UDP header
+ * @retval 0           Packet does not contain a UDP header
+ */
+int odp_packet_has_udp(odp_packet_t pkt);
+
+/**
+ * Check for TCP
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains a TCP header
+ * @retval 0           Packet does not contain a TCP header
+ */
+int odp_packet_has_tcp(odp_packet_t pkt);
+
+/**
+ * Check for SCTP
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains a SCTP header
+ * @retval 0           Packet does not contain a SCTP header
+ */
+int odp_packet_has_sctp(odp_packet_t pkt);
+
+/**
+ * Check for ICMP
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains an ICMP header
+ * @retval 0           Packet does not contain an ICMP header
+ */
+int odp_packet_has_icmp(odp_packet_t pkt);
+
+/**
+ * Check for packet flow hash
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains a hash value
+ * @retval 0           Packet does not contain a hash value
+ */
+int odp_packet_has_flow_hash(odp_packet_t pkt);
+
+/**
+ * Check for packet timestamp
+ *
+ * @param pkt          Packet handle
+ *
+ * @retval non-zero    Packet contains a timestamp value
+ * @retval 0           Packet does not contain a timestamp value
+ *
+ * @see odp_packet_has_ts_clr()
+ */
+int odp_packet_has_ts(odp_packet_t pkt);
+
+/**
+ * Set flag for L2 header, e.g. ethernet
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_l2_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for L3 header, e.g. IPv4, IPv6
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_l3_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for L4 header, e.g. UDP, TCP, SCTP (also ICMP)
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_l4_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for Ethernet header
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_eth_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for Ethernet broadcast address
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_eth_bcast_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for Ethernet multicast address
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_eth_mcast_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for jumbo frame
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_jumbo_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for VLAN
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_vlan_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for VLAN QinQ (stacked VLAN)
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_vlan_qinq_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for ARP
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_arp_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for IPv4
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_ipv4_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for IPv6
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_ipv6_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for IP broadcast address
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_ip_bcast_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for IP multicast address
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_ip_mcast_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for IP fragment
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_ipfrag_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for IP options
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_ipopt_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for IPSec
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_ipsec_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for UDP
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_udp_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for TCP
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_tcp_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for SCTP
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_sctp_set(odp_packet_t pkt, int val);
+
+/**
+ * Set flag for ICMP
+ *
+ * @param pkt Packet handle
+ * @param val Value
+ */
+void odp_packet_has_icmp_set(odp_packet_t pkt, int val);
+
+/**
+ * Clear flag for packet flow hash
+ *
+ * @param pkt Packet handle
+ *
+ * @note Set this flag is only possible through odp_packet_flow_hash_set()
+ */
+void odp_packet_has_flow_hash_clr(odp_packet_t pkt);
+
+/**
+ * Clear flag for packet timestamp
+ *
+ * This call clears the timestamp flag. A odp_packet_ts_set() call sets
+ * the flag in addition to the timestamp value.
+ *
+ * @param pkt Packet handle
+ *
+ * @see odp_packet_has_ts(), odp_packet_ts_set()
+ */
+void odp_packet_has_ts_clr(odp_packet_t pkt);
+
+/**
+ * @}
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#include <odp/visibility_end.h>
+#endif
